c0c2d4ebe8bc7cbc09aa1abf71663dfa589b1cc5
Rupinder <rupinder3972@gmail.com>
2022-01-22 23:42:16 +0530

    Removed non-required dependencies

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,9 @@
 import { createStore, applyMiddleware } from "redux"
-import logger from "redux-logger";
 import { persistStore } from "redux-persist";
 import thunk from "redux-thunk"
 import rootReducer from "./root-reducer";
 
 const middleWares = [thunk];
-
-if(process.env.NODE_ENV === 'development') {
-    middleWares.push(logger);
-}
-
 export const store = createStore(rootReducer, applyMiddleware(...middleWares));
 // for persisting state on reloads
 export const persistor = persistStore(store);
